0753bb422061ef0d393109af58a58fb15508e371
Benjamin Erhart <berhart@netzarchitekten.com>
2023-06-15 16:30:36 +0200

    Localize web templates.

--- OnionShare/WebServer.swift
+++ OnionShare/WebServer.swift
@@ -14,7 +14,18 @@ class WebServer: AdParticipesCumCepis.WebServer {
 
     private static var defaultContext: [String: Any] = [
         "app_name": Bundle.main.displayName,
-        "static_url_path": "/static"]
+        "static_url_path": "/static",
+
+        "error_header": NSLocalizedString("An error occurred", comment: "500.html"),
+        "unexpected_error": NSLocalizedString("Sorry, an unexpected error seems to have occurred, and your request didn't succeed.", comment: "500.html"),
+
+        "filename": NSLocalizedString("Filename", comment: "listing.html"),
+        "size": NSLocalizedString("Size", comment: "listing.html"),
+
+        "total_size": NSLocalizedString("Total size", comment: "send.html"),
+        "compressed": NSLocalizedString("compressed", comment: "send.html"),
+        "download_files": NSLocalizedString("Download Files", comment: "send.html"),
+    ]
 
 
     private let renderer: Environment
@@ -26,13 +37,13 @@ class WebServer: AdParticipesCumCepis.WebServer {
 
         super.init(localStaticPath: Bundle.main.path(forResource: "static", ofType: nil)!)
 
-        WebServer.defaultContext["static_url_path"] = staticPath.dropLast()
+        Self.defaultContext["static_url_path"] = staticPath.dropLast()
     }
 
 
     override func renderTemplate(name: String, context: [String : Any]) throws -> String {
         return try self.renderer.renderTemplate(
             name: name.appending(".html"),
-            context: WebServer.defaultContext.merging(context, uniquingKeysWith: { $1 }))
+            context: Self.defaultContext.merging(context, uniquingKeysWith: { $1 }))
     }
 }
